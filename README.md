1306abeb9ba55d975227ec4742d053e1206dec7d
zaplatynski <marian.zaplatynski@gmail.com>
2016-05-03 23:29:56 +0200

    Initial commit and working version

---
+++ README.md
@@ -0,0 +1,14 @@
+# Maven Package Type For FirstSpirit Modules
+
+This is a simple approach to create a Maven package type for FirstSpirit modules (FSM).
+
+The main goal is to simply a Maven pom and have working Maven life cycle with a working install and deploy phase.
+
+## How to use
+
+TODO... need more time to write down.
+
+##  Disclaimer
+
+By using it you agree to the license stated in the file [LICENSE](LICENSE). FirstSpirit is a trade mark by the [e-Spirit AG](http://www.e-spirit.com/).
+
---
+++ src/main/java/de/marza/firstspirit/modules/fsm/FsmMojo.java
@@ -0,0 +1,61 @@
+package de.marza.firstspirit.modules.fsm;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+
+/**
+ * The type Fsm mojo.
+ */
+@Mojo(name = "fsm", defaultPhase = LifecyclePhase.PACKAGE, threadSafe = true)
+public class FsmMojo extends AbstractMojo {
+
+  @Parameter(defaultValue = "${project.build.directory}/${project.build.finalName}.zip", required = true)
+  private File source;
+
+  @Parameter(defaultValue = "${project.build.directory}${project.build.finalName}.fsm", required = true)
+  private File target;
+
+  @Component
+  private MavenProject project;
+
+  public void execute() throws MojoExecutionException, MojoFailureException {
+
+    if (source == null) {
+      throw new MojoFailureException("source is null");
+    }
+
+    if (target == null) {
+      throw new MojoFailureException("target is null");
+    }
+
+    if (!project.getAttachedArtifacts().isEmpty()) {
+      throw new MojoFailureException("Configure the maven assembly plugin not to attach any files!");
+    }
+
+    getLog().info("Renaming " + source.getName() + " to " + target.getName() + "...");
+
+    final boolean success;
+    try {
+      success = source.renameTo(target);
+    } catch (final Exception e) {
+      getLog().error(e);
+      throw new MojoExecutionException("Renaming to *.fsm failed: " + e.toString(), e);
+    }
+
+    if (success) {
+      getLog().info("Success! Attach artifact...");
+      project.getArtifact().setFile(target);
+    } else {
+      getLog().error("Renaming to *.fsm failed (reason unkown)");
+      throw new MojoFailureException("Renaming to *.fsm failed (reason unkown)");
+    }
+  }
+}
